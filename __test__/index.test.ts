71ea54d3d36b5438e49773d0c37cd1431178859a
duxiuxiu <duxx@summit.com.cn>
2019-05-24 20:06:55 +0800

    continue: 实例 fizzBuzz should be defined

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -5,5 +5,16 @@ describe('测试 FizzBuzz 类', () => {
     test('FizzBuzz should be defined', () => {
         expect(FizzBuzz).toBeDefined()
     })
+
+    let fizzBuzz : IFizzBuzz
+
+    beforeEach(() => {
+        fizzBuzz = new FizzBuzz()
+        return fizzBuzz
+    })
+    test('fizzBuzz should be defined', () => {
+        expect(fizzBuzz).toBeDefined()
+    })
+
     
 })
